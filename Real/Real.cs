c3ebd19dce71e44b0db09dc253016caefddbaed8
kimiko88 <chimien@libero.it>
2015-02-26 11:10:20 +0100

    Other useful division method

--- Real/Real.cs
+++ Real/Real.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 
 namespace Real
 {
     [Serializable]
-    public struct RealNumber : IFormattable, IComparable, IComparable<RealNumber>, IEquatable<RealNumber>
+    public struct RealNumber : IFormattable, IComparable<RealNumber>, IEquatable<RealNumber>
     {
         private int Exponent { get; set; }
 
         public BigInteger Number { get; set; }
 
-        public RealNumber(string number) : this()
+        public RealNumber(string number)
+            : this()
         {
             BigInteger Number;
             number.SingleOrDefault(t => t == '.');
@@ -23,7 +25,7 @@ namespace Real
             {
                 Exponent = 0;
             }
-            if (BigInteger.TryParse(number.Replace(".",""), out Number))
+            if (BigInteger.TryParse(number.Replace(".", ""), out Number))
             {
                 this.Number = Number;
             }
@@ -33,19 +35,21 @@ namespace Real
             }
         }
 
-        public RealNumber(BigInteger number):this()
+        public RealNumber(BigInteger number)
+            : this()
         {
             this.Number = number;
             this.Exponent = 0;
         }
 
 
-        public RealNumber(BigInteger number,int expo)
+        public RealNumber(BigInteger number, int expo)
             : this()
         {
             this.Number = number;
             this.Exponent = expo;
         }
+
 
         public override string ToString()
         {
@@ -54,7 +58,23 @@ namespace Real
             {
                 return num.Substring(0, num.Length - Exponent) + '.' + num.Substring(num.Length - Exponent);
             }
-            else if(Exponent >= num.Length)
+            else if (Exponent >= num.Length)
+            {
+                var diff = Exponent - num.Length;
+                return "0." + new string('0', diff) + num.Substring(0, num.Length);
+            }
+            return num;
+        }
+
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            var num = Number.ToString();
+            if (this.Exponent > 0 && this.Exponent < num.Length)
+            {
+                return num.Substring(0, num.Length - Exponent) + '.' + num.Substring(num.Length - Exponent);
+            }
+            else if (Exponent >= num.Length)
             {
                 var diff = Exponent - num.Length;
                 return "0." + new string('0', diff) + num.Substring(0, num.Length);
@@ -68,24 +88,17 @@ namespace Real
             throw new NotImplementedException();
         }
 
+
         public int CompareTo(RealNumber other)
         {
-            throw new NotImplementedException();
+            BalanceNumbers(ref other, ref this);
+            return this.Number.CompareTo(other);
         }
+
 
         public bool Equals(RealNumber other)
         {
             return other.Number == Number;
-        }
-
-        private static RealNumber BalanceNumber(RealNumber a,int Exponent)
-        {
-            if (a.Exponent < Exponent && Exponent > 0)
-            {
-                var diff = Exponent - a.Exponent;
-                a.Number = a.Number* BigInteger.Pow(10,diff);
-            }
-            return a;
         }
 
 
@@ -95,16 +108,8 @@ namespace Real
             {
                 return new RealNumber(a.Number + b.Number);
             }
-                if (a.Exponent < b.Exponent)
-                {
-                    a = BalanceNumber(a, b.Exponent);
-                }
-                else if (b.Exponent < a.Exponent)
-                {
-                    b = BalanceNumber(b, a.Exponent);
-                }
-
-                return new RealNumber(a.Number + b.Number, Max(a.Exponent,b.Exponent));
+            BalanceNumbers(ref a, ref b);
+            return new RealNumber(a.Number + b.Number, Max(a.Exponent, b.Exponent));
         }
 
 
@@ -114,16 +119,8 @@ namespace Real
             {
                 return new RealNumber(a.Number - b.Number);
             }
-                if (a.Exponent < b.Exponent)
-                {
-                    a = BalanceNumber(a, b.Exponent);
-                }
-                else if (b.Exponent < a.Exponent)
-                {
-                    b = BalanceNumber(b, a.Exponent);
-                }
-
-                return new RealNumber(a.Number - b.Number, Max(a.Exponent, b.Exponent));
+            BalanceNumbers(ref a, ref b);
+            return new RealNumber(a.Number - b.Number, Max(a.Exponent, b.Exponent));
         }
 
 
@@ -133,16 +130,8 @@ namespace Real
             {
                 return new RealNumber(a.Number * b.Number);
             }
-                if (a.Exponent < b.Exponent)
-                {
-                    a = BalanceNumber(a, b.Exponent);
-                }
-                else if (b.Exponent < a.Exponent)
-                {
-                    b = BalanceNumber(b, a.Exponent);
-                }
-
-                return new RealNumber(a.Number * b.Number, 2*Max(a.Exponent, b.Exponent));
+            BalanceNumbers(ref a, ref b);
+            return new RealNumber(a.Number * b.Number, 2 * Max(a.Exponent, b.Exponent));
         }
 
 
@@ -154,34 +143,55 @@ namespace Real
             }
             if (a.Exponent <= 0 && b.Exponent <= 0)
             {
-                return  new RealNumber(SetCommaDiv(Divide(a, b),a.Exponent - b.Exponent));
+                return new RealNumber(SetCommaDiv(Divide(a, b), a.Exponent - b.Exponent));
             }
-                if (a.Exponent < b.Exponent)
-                {
-                    a = BalanceNumber(a, b.Exponent);
-                }
-                else if (b.Exponent < a.Exponent)
-                {
-                    b = BalanceNumber(b, a.Exponent);
-                }
-
-                return new RealNumber(SetCommaDiv(Divide(a, b),0));
+            BalanceNumbers(ref a, ref b);
+            return new RealNumber(SetCommaDiv(Divide(a, b), 0));
         }
 
-        public static string Divide(RealNumber a, RealNumber b, int precision = 15)
+
+        public static RealNumber Division(RealNumber a, RealNumber b, int prec)
+        {
+            if (b.Number.IsZero)
+            {
+                throw new DivideByZeroException("Zero division error");
+            }
+            if (a.Exponent <= 0 && b.Exponent <= 0)
+            {
+                return new RealNumber(SetCommaDiv(Divide(a, b, prec), a.Exponent - b.Exponent));
+            }
+            BalanceNumbers(ref a, ref b);
+            return new RealNumber(SetCommaDiv(Divide(a, b, prec), 0));
+        }
+
+        public static RealNumber PeriodicDivision(RealNumber a, RealNumber b, int prec)
+        {
+            if (b.Number.IsZero)
+            {
+                throw new DivideByZeroException("Zero division error");
+            }
+            if (a.Exponent <= 0 && b.Exponent <= 0)
+            {
+                return new RealNumber(SetCommaDiv(DividePeriodic(a, b, prec), a.Exponent - b.Exponent));
+            }
+            BalanceNumbers(ref a, ref b);
+            return new RealNumber(SetCommaDiv(DividePeriodic(a, b, prec), 0));
+        }
+
+        private static string Divide(RealNumber a, RealNumber b, int precision = 15)
         {
             string result = "";
             BigInteger rem = 1;
-            int i = 1;
+            bool i = true;
             var num = a.Number;
             var denom = b.Number;
-            while (!(result.ToString().Length - result.ToString().IndexOf('.') > precision) && !rem.IsZero)
+            while (!(result.Length - result.IndexOf('.') > precision) && !rem.IsZero)
             {
                 result = String.Concat(result, BigInteger.DivRem(num, denom, out rem));
-                if (i == 1)
+                if (i)
                 {
                     result = string.Concat(result, '.');
-                    i++;
+                    i = false;
                 }
                 if (!rem.IsZero)
                 {
@@ -201,7 +211,46 @@ namespace Real
             return result;
         }
 
-        public static string SetCommaDiv(string result, int maxexp)
+        private static string DividePeriodic(RealNumber a, RealNumber b, int precision = 15)
+        {
+            string result = "";
+            BigInteger rem = 1;
+            bool i = true;
+            var num = a.Number;
+            var denom = b.Number;
+            var remList = new List<BigInteger>();
+            while (!(result.Length - result.IndexOf('.') > precision) && !rem.IsZero)
+            {
+                result = String.Concat(result, BigInteger.DivRem(num, denom, out rem));
+                if (remList.Contains(rem)) { break;}
+                remList.Add(rem);
+                if (i)
+                {
+                    result = string.Concat(result, '.');
+                    i = false;
+                }
+                if (!rem.IsZero)
+                {
+                    num = BigInteger.Multiply(rem, 10);
+                    if ((denom.ToString().Length - rem.ToString().Length - 1) > 0)
+                    {
+                        num = BigInteger.Multiply(num,
+                            BigInteger.Pow(10, denom.ToString().Length - rem.ToString().Length - 1));
+                        result =
+                            result.PadRight(result.Length + (denom.ToString().Length - rem.ToString().Length - 1),
+                                '0');
+                    }
+                    if (BigInteger.Compare(num, denom) < 0)
+                    {
+                        num = BigInteger.Multiply(num, 10);
+                        result = string.Concat(result, '0');
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static string SetCommaDiv(string result, int maxexp)
         {
             string res = "";
             var comma = result.IndexOf('.');
@@ -226,17 +275,32 @@ namespace Real
             return res;
         }
 
-
-
-
-        private static  int Max(int expo1,int expo2)
+        private static int Max(int expo1, int expo2)
         {
             return expo1 < expo2 ? expo2 : expo1;
         }
 
-        public string ToString(string format, IFormatProvider formatProvider)
+        private static RealNumber BalanceNumber(RealNumber a, int Exponent)
         {
-            throw new NotImplementedException();
+            if (a.Exponent < Exponent && Exponent > 0)
+            {
+                var diff = Exponent - a.Exponent;
+                a.Number = a.Number * BigInteger.Pow(10, diff);
+            }
+            return a;
         }
+
+        private static void BalanceNumbers(ref RealNumber a, ref RealNumber b)
+        {
+            if (a.Exponent < b.Exponent)
+            {
+                a = BalanceNumber(a, b.Exponent);
+            }
+            else if (b.Exponent < a.Exponent)
+            {
+                b = BalanceNumber(b, a.Exponent);
+            }
+        }
+
     }
 }
--- Testing/Test.cs
+++ Testing/Test.cs
@@ -16,13 +16,7 @@ namespace Testing
             _sut = new RealNumber();
         }
 
-        [Test]
-        public void Should_Sum_Right_BigInteger_Case()
-        {
-            var num1 = new RealNumber("1");
-            var num2 = new RealNumber("99999");
-            Assert.AreEqual((num1+num2).ToString(), "100000");
-        }
+        #region conversion test
 
         [Test]
         public void Should_String_Right()
@@ -30,7 +24,6 @@ namespace Testing
             var num1 = new RealNumber("1.10");
             Assert.AreEqual(num1.ToString(), "1.10");
         }
-
 
         [Test]
         public void Should_String_Right1()
@@ -75,7 +68,17 @@ namespace Testing
             var num1 = new RealNumber("93124.1a0");
             Assert.AreEqual(num1.ToString(), "93124.10");
         }
+        #endregion
 
+        #region summation test
+
+        [Test]
+        public void Should_Sum_Right_BigInteger_Case()
+        {
+            var num1 = new RealNumber("1");
+            var num2 = new RealNumber("99999");
+            Assert.AreEqual((num1 + num2).ToString(), "100000");
+        }
 
         [Test]
         public void Should_Sum_Right_Real_Case()
@@ -118,7 +121,6 @@ namespace Testing
             Assert.AreEqual((num1 + num2).ToString(), "10000.00111011");
         }
 
-
         [Test]
         public void Should_Sum_Right_Real_Case5()
         {
@@ -126,7 +128,9 @@ namespace Testing
             var num2 = new RealNumber("9999");
             Assert.AreEqual((num1 + num2).ToString(), "9999.00111111");
         }
+        #endregion
 
+        #region subtraction test
         [Test]
         public void Should_Subtract_Right_BigInteger_Case()
         {
@@ -159,7 +163,6 @@ namespace Testing
             Assert.AreEqual((num1 - num2).ToString(), "-99979.890");
         }
 
-
         [Test]
         public void Should_Subtract_Right_Real_Case2()
         {
@@ -184,7 +187,6 @@ namespace Testing
             Assert.AreEqual((num1 - num2).ToString(), "-9999.99888789");
         }
 
-
         [Test]
         public void Should_Subtract_Right_Real_Case5()
         {
@@ -208,8 +210,9 @@ namespace Testing
             var num2 = new RealNumber("0.00111111");
             Assert.AreEqual((num1 - num2).ToString(), "0.00888889");
         }
+        #endregion
 
-
+        #region multiplication test
         [Test]
         public void Should_Multiply_Right_BigInteger_Case()
         {
@@ -217,7 +220,6 @@ namespace Testing
             var num2 = new RealNumber("99999");
             Assert.AreEqual((num1 * num2).ToString(), "99999");
         }
-
 
         [Test]
         public void Should_Multiply_Right_Real_Case()
@@ -234,7 +236,6 @@ namespace Testing
             var num2 = new  RealNumber("99990");
             Assert.AreEqual((num1 * num2).ToString(), "1010898.900000");
         }
-
 
         [Test]
         public void Should_Multiply_Right_Real_Case2()
@@ -260,7 +261,6 @@ namespace Testing
             Assert.AreEqual((num1 * num2).ToString(), "11.1110999988888900");
         }
 
-
         [Test]
         public void Should_Multiply_Right_Real_Case5()
         {
@@ -284,8 +284,9 @@ namespace Testing
             var num2 = new RealNumber("0.00111111");
             Assert.AreEqual((num1 * num2).ToString(), "0.0000111111000000");
         }
+        #endregion
 
-
+        #region division operator test
         [Test]
         public void Should_Divide_Right_BigInteger_Case()
         {
@@ -293,7 +294,6 @@ namespace Testing
             var num2 = new RealNumber("99999");
             Assert.AreEqual((num1 / num2).ToString(), "0.0000100001000010000");
         }
-
 
         [Test]
         public void Should_Divide_Right_Real_Case()
@@ -311,7 +311,6 @@ namespace Testing
             var num2 = new RealNumber("99990");
             Assert.AreEqual((num1 / num2).ToString(), "0.000101110111011");
         }
-
 
         [Test]
         public void Should_Divide_Right_Real_Case2()
@@ -337,7 +336,6 @@ namespace Testing
             Assert.AreEqual((num1 / num2).ToString(), "0.0000001111110000");
         }
 
-
         [Test]
         public void Should_Divide_Right_Real_Case5()
         {
@@ -361,5 +359,185 @@ namespace Testing
             var num2 = new RealNumber("0.00111111");
             Assert.AreEqual((num1 / num2).ToString(), "9.00000900000900000");
         }
+
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Should_Should_Throw_Exception_When_Divide_By_Zero()
+        {
+            var num = new RealNumber("9");
+            var denom = new RealNumber("0");
+            (num/denom).ToString();
+        }
+        #endregion
+
+        #region division method with precision test
+
+        [Test]
+        public void Should_Divide_Right_BigInteger_Case_With_Precision()
+        {
+            var num1 = new RealNumber("1");
+            var num2 = new RealNumber("99999");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.000010000100001000010000100001000010000100001000010000");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case_With_Precision()
+        {
+            var num1 = new RealNumber("1.10");
+            var num2 = new RealNumber("99999");
+            var pp = (num1 / num2).ToString();
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.00001100011000110001100011000110001100011000110001");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case1_With_Precision()
+        {
+            var num1 = new RealNumber("10.110");
+            var num2 = new RealNumber("99990");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.00010111011101110111011101110111011101110111011101");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case2_With_Precision()
+        {
+            var num1 = new RealNumber("1111.11111");
+            var num2 = new RealNumber("9999.999999");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.11111111101111111110111111111011111111101111111110");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case3_With_Precision()
+        {
+            var num1 = new RealNumber("0.11111");
+            var num2 = new RealNumber("9999.999999");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.000011111000001111100000111110000011111000001111100000");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case4_With_Precision()
+        {
+            var num1 = new RealNumber("0.00111111");
+            var num2 = new RealNumber("9999.999999");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.00000011111100001111110000111111000011111100001111");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case5_With_Precision()
+        {
+            var num1 = new RealNumber("0.00111111");
+            var num2 = new RealNumber("9999");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "0.00000011112211221122112211221122112211221122112211");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case6_With_Precision()
+        {
+            var num1 = new RealNumber("9999");
+            var num2 = new RealNumber("0.00111111");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "8999108.99910899910899910899910899910899910899910899910899");
+        }
+
+        [Test]
+        public void Should_Divide_Right_Real_Case7_With_Precision()
+        {
+            var num1 = new RealNumber("0.01");
+            var num2 = new RealNumber("0.00111111");
+            Assert.AreEqual(RealNumber.Division(num1, num2, 50).ToString(), "9.00000900000900000900000900000900000900000900000900000");
+        }
+
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Should_Should_Throw_Exception_When_Divide_By_Zero_With_Precision()
+        {
+            var num = new RealNumber("9");
+            var denom = new RealNumber("0");
+            RealNumber.Division(num, denom, 50).ToString();
+        }
+        #endregion
+
+        #region periodicDivision method with precision test
+
+        [Test]
+        public void Should_PeriodicDivide_Right_BigInteger_Case_With_Precision()
+        {
+            var num1 = new RealNumber("1");
+            var num2 = new RealNumber("99999");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.00001");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case_With_Precision()
+        {
+            var num1 = new RealNumber("1.10");
+            var num2 = new RealNumber("99999");
+            var pp = (num1 / num2).ToString();
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.0000110001");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case1_With_Precision()
+        {
+            var num1 = new RealNumber("10.110");
+            var num2 = new RealNumber("99990");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.00010111");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case2_With_Precision()
+        {
+            var num1 = new RealNumber("1111.11111");
+            var num2 = new RealNumber("9999.999999");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.11111111101");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case3_With_Precision()
+        {
+            var num1 = new RealNumber("0.11111");
+            var num2 = new RealNumber("9999.999999");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.000011111000001");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case4_With_Precision()
+        {
+            var num1 = new RealNumber("0.00111111");
+            var num2 = new RealNumber("9999.999999");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.00000011111100001");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case5_With_Precision()
+        {
+            var num1 = new RealNumber("0.00111111");
+            var num2 = new RealNumber("9999");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "0.000000111122");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case6_With_Precision()
+        {
+            var num1 = new RealNumber("9999");
+            var num2 = new RealNumber("0.00111111");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "8999108.999108");
+        }
+
+        [Test]
+        public void Should_PeriodicDivide_Right_Real_Case7_With_Precision()
+        {
+            var num1 = new RealNumber("0.01");
+            var num2 = new RealNumber("0.00111111");
+            Assert.AreEqual(RealNumber.PeriodicDivision(num1, num2, 50).ToString(), "9.000009");
+        }
+
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Should_PeriodicDivide_Throw_Exception_When_Divide_By_Zero_With_Precision()
+        {
+            var num = new RealNumber("9");
+            var denom = new RealNumber("0");
+            RealNumber.PeriodicDivision(num, denom, 50).ToString();
+        }
+        #endregion
     }
 }
